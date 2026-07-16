aa259c6d8dddf9557ae7fe8523951764bbd26ae7
KaitoKariheddo <95089439+KaitoKariheddo@users.noreply.github.com>
2026-07-16 14:03:12 +0200

    Update bigscreen-installer.sh

--- bigscreen-installer.sh
+++ bigscreen-installer.sh
@@ -34,22 +34,22 @@ final_disk="/dev/${raw_disk%% *}" #generate path for partioning and formating
 #password
 ###############################################################################
 
-while true; do
-PASSWORD=$(whiptail --passwordbox "please enter your secret password" 8 78 --title "password dialog" 3>&1 1>&2 2>&3)
+#while true; do
+#PASSWORD=$(whiptail --passwordbox "please enter your secret password" 8 78 --title "password dialog" 3>&1 1>&2 2>&3)
 
-PASSWORD2=$(whiptail --passwordbox "please repeat" 8 78 --title "password control" 3>&1 1>&2 2>&3)
+#PASSWORD2=$(whiptail --passwordbox "please repeat" 8 78 --title "password control" 3>&1 1>&2 2>&3)
 
-if [ -z "$PASSWORD" ] || [ -z "$PASSWORD2" ]; then
-  whiptail --msgbox "Can't be empty." 10 60
-  continue
-fi
+#if [ -z "$PASSWORD" ] || [ -z "$PASSWORD2" ]; then
+#  whiptail --msgbox "Can't be empty." 10 60
+#  continue
+#fi
 
-if [[ "$PASSWORD" = "$PASSWORD2" ]]; then
- break
-else
- whiptail --msgbox "Input wrong, please repeat." 10 60
-  fi
-done
+#if [[ "$PASSWORD" = "$PASSWORD2" ]]; then
+# break
+#else
+# whiptail --msgbox "Input wrong, please repeat." 10 60
+#  fi
+#done
 
 ###############################################################################
 #Steam installation
@@ -68,7 +68,8 @@ bash "$dir/installer.sh" $final_disk $steaminstall
 
 cp "$dir/configurator.sh" /mnt
 chmod 755 /mnt/configurator.sh
-arch-chroot /mnt ./configurator.sh $steaminstall $PASSWORD
+arch-chroot /mnt ./configurator.sh $steaminstall
+#arch-chroot /mnt ./configurator.sh $steaminstall $PASSWORD
 
 rm /mnt/configurator.sh
 umount /mnt/boot
